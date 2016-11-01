4e3f63577b58432cb2c0b4575527a3aa943576bf
jkaupp <jkaupp@gmail.com>
2016-11-01 14:09:42 -0400

    Added prog_from_plan

--- R/utils.R
+++ R/utils.R
@@ -14,3 +14,21 @@ grid_draw <- function(x) {
   grid::grid.draw(x)
 
 }
+
+
+#' prog_from_plan
+#'
+#' @param x plan string
+#'
+#' @return prog
+#' @export
+prog_from_plan <- function(x) {
+
+  dplyr::case_when(grepl("CHEE", x) ~ "CHEE",
+                   grepl("MECH|MEME",x) ~ "MECH",
+                   grepl("ECEN|ELEC|CMPE", x) ~ "ECE",
+                   grepl("MINE", x) ~ "MINE",
+                   grepl("CIVL", x) ~ "CIVL")
+
+
+}
