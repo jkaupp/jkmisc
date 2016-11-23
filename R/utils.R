5dc547cea204425d385a012d692b38fef0f0ed37
jkaupp <jkaupp@gmail.com>
2016-11-23 10:29:39 -0500

    Added remove_empty_rows from janitor

--- R/utils.R
+++ R/utils.R
@@ -57,6 +57,27 @@ clean_names <- function(dat){
 }
 
 
+
+#' @title Removes empty rows from a data.frame.
+#'
+#' @description
+#' Removes all rows from a data.frame that are composed entirely of \code{NA} values.
+#' From Janitor
+#'
+#' @param dat the input data.frame.
+#' @return Returns the data.frame with no empty rows.
+#' @export
+#' @examples
+#' # called with magrittr pipe %>% :
+#' # library(dplyr)
+#' # not run:
+#' # dat %>% remove_empty_rows
+
+remove_empty_rows <- function(dat){
+  dat[rowSums(is.na(dat)) != ncol(dat), ]
+}
+
+
 #' prog_from_plan
 #'
 #' @param x plan string
