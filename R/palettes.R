13ed36d77e36e583be4ec4fb33bdd9663efe4ae7
jkaupp <jkaupp@gmail.com>
2017-11-24 10:41:00 -0500

    Updated documentation

--- R/palettes.R
+++ R/palettes.R
@@ -1,57 +1,72 @@
+#' Harris Mountain Form color palette
+#' @export
+mountain_pal <- c("#184860", "#486078","#d8d8d8", "#484860", "#181830")
+
 #' Lumina color palette
 #' @export
-.lumina <- c("#EDDAEB","#AD8CAE","#4F93B8","#306489","#222B4C")
+lumina_pal <- c("#EDDAEB","#AD8CAE","#4F93B8","#306489","#222B4C")
 
 #' Queen's tri-color palette
 #' @export
-.tricolour <- c("#9d1939", "#eebd31", "#11335d")
+tricolour_pal <- c("#9d1939", "#eebd31", "#11335d")
 
 #' Graduate Attribute Colors
 #' @export
-.ga_colors <- c("#E12493", "#067F0A", "#08829F", "#F0A305", "#9E56E5", "#845508", "#B21C42", "#214A1E", "#B8AACC", "#97B438", "#B988EF", "#12B2B3") %>%
+ga_pal <- c("#E12493", "#067F0A", "#08829F", "#F0A305", "#9E56E5", "#845508", "#B21C42", "#214A1E", "#B8AACC", "#97B438", "#B988EF", "#12B2B3") %>%
   stats::setNames(c("KB","PA","IN","DE","ET","TW","CO","PR","IM","EE","EC","LL"))
 
 #' AU Colors
 #' @export
-.au_colors <- RColorBrewer::brewer.pal(5, "Set1") %>%
+au_pal <- RColorBrewer::brewer.pal(5, "Set1") %>%
   stats::setNames(c("Math","NS","CS","ES","ED"))
 
-# FOCUS PALETTES
-# Red as highlight
-#' @export
-.redfocus <- c("#CB181D", "#252525", "#525252", "#737373", "#969696", "#BDBDBD", "#D9D9D9", "#F0F0F0")
 
-# Green as highlight
+#' Red as highlight
 #' @export
-.greenfocus <- c("#41AB5D", "#252525", "#525252", "#737373", "#969696", "#BDBDBD", "#D9D9D9", "#F0F0F0")
+redfocus <- c("#CB181D", "#252525", "#525252", "#737373", "#969696", "#BDBDBD", "#D9D9D9", "#F0F0F0")
 
-# Blue as highlight
+#' Green as highlight
 #' @export
-.bluefocus <- c("#0033FF", "#252525", "#525252", "#737373", "#969696", "#BDBDBD", "#D9D9D9", "#F0F0F0")
+greenfocus <- c("#41AB5D", "#252525", "#525252", "#737373", "#969696", "#BDBDBD", "#D9D9D9", "#F0F0F0")
+
+#' Blue as highlight
+#' @export
+bluefocus <- c("#0033FF", "#252525", "#525252", "#737373", "#969696", "#BDBDBD", "#D9D9D9", "#F0F0F0")
 
 
-# Qualitative color schemes by Paul Tol
+#' Qualitative color schemes by Paul Tol
 #' @export
-.tol1qualitative <- c("#4477AA")
+tol1qualitative <- c("#4477AA")
+#' Qualitative color schemes by Paul Tol
 #' @export
-.tol2qualitative <- c("#4477AA", "#CC6677")
+tol2qualitative <- c("#4477AA", "#CC6677")
+#' Qualitative color schemes by Paul Tol
 #' @export
-.tol3qualitative <- c("#4477AA", "#DDCC77", "#CC6677")
+tol3qualitative <- c("#4477AA", "#DDCC77", "#CC6677")
+#' Qualitative color schemes by Paul Tol
 #' @export
-.tol4qualitative <- c("#4477AA", "#117733", "#DDCC77", "#CC6677")
+tol4qualitative <- c("#4477AA", "#117733", "#DDCC77", "#CC6677")
+#' Qualitative color schemes by Paul Tol
 #' @export
-.tol5qualitative <- c("#332288", "#88CCEE", "#117733", "#DDCC77", "#CC6677")
+tol5qualitative <- c("#332288", "#88CCEE", "#117733", "#DDCC77", "#CC6677")
+#' Qualitative color schemes by Paul Tol
 #' @export
-.tol6qualitative <- c("#332288", "#88CCEE", "#117733", "#DDCC77", "#CC6677","#AA4499")
+tol6qualitative <- c("#332288", "#88CCEE", "#117733", "#DDCC77", "#CC6677","#AA4499")
+#' Qualitative color schemes by Paul Tol
 #' @export
-.tol7qualitative <- c("#332288", "#88CCEE", "#44AA99", "#117733", "#DDCC77", "#CC6677","#AA4499")
+tol7qualitative <- c("#332288", "#88CCEE", "#44AA99", "#117733", "#DDCC77", "#CC6677","#AA4499")
+#' Qualitative color schemes by Paul Tol
 #' @export
-.tol8qualitative <- c("#332288", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#CC6677","#AA4499")
+tol8qualitative <- c("#332288", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#CC6677","#AA4499")
+#' Qualitative color schemes by Paul Tol
 #' @export
-.tol9qualitative <- c("#332288", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#CC6677", "#882255", "#AA4499")
+tol9qualitative <- c("#332288", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#CC6677", "#882255", "#AA4499")
+#' Qualitative color schemes by Paul Tol
 #' @export
-.tol10qualitative <- c("#332288", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#661100", "#CC6677", "#882255", "#AA4499")
+tol10qualitative <- c("#332288", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#661100", "#CC6677", "#882255", "#AA4499")
+#' Qualitative color schemes by Paul Tol
 #' @export
-.tol11qualitative <- c("#332288", "#6699CC", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#661100", "#CC6677", "#882255", "#AA4499")
+tol11qualitative <- c("#332288", "#6699CC", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#661100", "#CC6677", "#882255", "#AA4499")
+#' Qualitative color schemes by Paul Tol
 #' @export
-.tol12qualitative <-c("#332288", "#6699CC", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#661100", "#CC6677", "#AA4466", "#882255", "#AA4499")
+tol12qualitative <- c("#332288", "#6699CC", "#88CCEE", "#44AA99", "#117733", "#999933", "#DDCC77", "#661100", "#CC6677", "#AA4466", "#882255", "#AA4499")
